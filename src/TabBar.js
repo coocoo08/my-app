1f7fd8a5b780ccb736b09c99c2f41b3a81d002ab
coocoo08 <futurely08@naver.com>
2023-07-23 13:27:21 +0900

    UI 개발(4) 탭바

---
+++ src/TabBar.js
@@ -0,0 +1,72 @@
+import { TouchableOpacity, View } from "react-native"
+import { Fontisto, Ionicons } from '@expo/vector-icons';
+import { getBottomSpace } from "react-native-iphone-x-helper";
+
+const bottomSpace = getBottomSpace();
+
+const TabButton = ({ 
+    isSelected, 
+    onPress, 
+    activeIconName, 
+    inactiveIconName, 
+    isIconFontisto,
+    isIconOinicons 
+}) => {
+    return(
+        <TouchableOpacity 
+            activeOpacity={1}
+            onPress={onPress} 
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                paddingVertical: 10,
+
+            }}
+        >
+            {isIconFontisto && <Fontisto name={isSelected ? activeIconName : inactiveIconName} size={24} color="black" />}
+            {isIconOinicons && <Ionicons name={isSelected ? activeIconName : inactiveIconName} size={24} color="black" />}
+        </TouchableOpacity>
+    )
+}
+
+export default ( { selectedTabIdx, setSelectedTabIdx }) => {
+    return(
+        <View style={{ 
+            flexDirection:"row", 
+            width:"100%", 
+            paddingBottom: bottomSpace,
+            borderTopWidth: 0.5,
+            borderTopColor: "grey"
+            }}>
+            <TabButton
+                isSelected={selectedTabIdx === 0}
+                onPress={() => setSelectedTabIdx(0)}
+                activeIconName={"person"}
+                inactiveIconName={"persons"}
+                isIconFontisto
+            />
+            <TabButton
+                isSelected={selectedTabIdx === 1}
+                onPress={() => setSelectedTabIdx(1)}
+                activeIconName={"chatbubble"}
+                inactiveIconName={"chatbubble-outline"}
+                isIconOinicons
+            />
+            <TabButton
+                isSelected={selectedTabIdx === 2}
+                onPress={() => setSelectedTabIdx(2)}
+                activeIconName={"pricetag"}
+                inactiveIconName={"pricetag-outline"}
+                isIconOinicons
+            />
+            <TabButton
+                isSelected={selectedTabIdx === 3}
+                onPress={() => setSelectedTabIdx(3)}
+                activeIconName={"add-circle"}
+                inactiveIconName={"add-circle-outline"}
+                isIconOinicons
+            />
+        </View>
+    )    
+}
